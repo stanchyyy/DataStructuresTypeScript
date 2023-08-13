2c594954f7c7d74d516fb86d93090dccdd5ec96e
stanislavatanasov <stanchyyy@gmail.com>
2023-08-14 01:44:48 +0300

    added additional files

--- data-structures-typescript/src/App.tsx
+++ data-structures-typescript/src/App.tsx
@@ -1,29 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import ErrorPage from './routes/error-page';
+import Array from './routes/array';
+import Root from "./routes/root"
+import LinkedList from './routes/linked-list';
+import BinarySearchTree from './routes/binary-search-tree';
+import BinaryTree from './routes/binary-tree';
+import Graph from './routes/graph';
+import Hashing from './routes/hashing';
+import Heap from './routes/heap';
+import Matrix from './routes/matrix';
+import Queue from './routes/queue';
+import Stack from './routes/stack';
+import { ArrayLoader } from './Loaders/array-loader';
+import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+let childrenRoutes : RouteObject []=[
+  {
+    path: "array",
+    element : <Array/>,
+    loader : ArrayLoader,
+  },
+  {
+    path: "linkedList",
+    element : <LinkedList/>,
+  },
+  {
+    path: "binarytree",
+    element : <BinaryTree/>,
+  },
+  {
+    path: "binarysearchtree",
+    element : <BinarySearchTree/>,
+  },
+  {
+    path: "graph",
+    element : <Graph/>,
+  },
+  {
+    path: "hashing",
+    element : <Hashing/>,
+  },
+  {
+    path: "heap",
+    element : <Heap/>,
+  },
+  {
+    path: "matrix",
+    element : <Matrix/>,
+  },
+  {
+    path: "queue",
+    element : <Queue/>,
+  },
+  {
+    path: "stack",
+    element : <Stack />,
+  },
+
+]
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement:<ErrorPage/>,
+    children : childrenRoutes,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- data-structures-typescript/src/index.tsx
+++ data-structures-typescript/src/index.tsx
@@ -2,82 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, RouteObject, RouterProvider,} from "react-router-dom";
-import ErrorPage from './routes/error-page';
-import Array from './routes/array';
-import Root from "./routes/root"
-import LinkedList from './routes/linked-list';
-import BinarySearchTree from './routes/binary-search-tree';
-import BinaryTree from './routes/binary-tree';
-import Graph from './routes/graph';
-import Hashing from './routes/hashing';
-import Heap from './routes/heap';
-import Matrix from './routes/matrix';
-import Queue from './routes/queue';
-import Stack from './routes/stack';
-import { ArrayLoader } from './Loaders/array-loader';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-let childrenRoutes : RouteObject []=[
-  {
-    path: "array",
-    element : <Array/>,
-    loader : ArrayLoader,
-  },
-  {
-    path: "linkedList",
-    element : <LinkedList/>,
-  },
-  {
-    path: "binarytree",
-    element : <BinaryTree/>,
-  },
-  {
-    path: "binarysearchtree",
-    element : <BinarySearchTree/>,
-  },
-  {
-    path: "graph",
-    element : <Graph/>,
-  },
-  {
-    path: "hashing",
-    element : <Hashing/>,
-  },
-  {
-    path: "heap",
-    element : <Heap/>,
-  },
-  {
-    path: "matrix",
-    element : <Matrix/>,
-  },
-  {
-    path: "queue",
-    element : <Queue/>,
-  },
-  {
-    path: "stack",
-    element : <Stack />,
-  },
-
-]
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement:<ErrorPage/>,
-    children : childrenRoutes,
-  },
-]);
-
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>
 );
 
